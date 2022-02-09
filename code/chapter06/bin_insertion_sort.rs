bf625e6510f06ede81439ff5519d8faa78a17f28
Shieber <QMH_XB_FLTMY@yahoo.com>
2022-02-09 13:00:14 +0800

    fix: fix subtract with overflow panic

--- code/chapter06/bin_insertion_sort.rs
+++ code/chapter06/bin_insertion_sort.rs
@@ -10,13 +10,14 @@ fn bin_insertion_sort(nums: &mut [i32]) {
     for i in 1..nums.len() {
         left = 0;       // 已排序数组左右边界
         right = i - 1;
-
         temp = nums[i]; // 待排序数据
 
         // 二分法找到 temp 的位置
         while left <= right {
             mid = (left + right) >> 1;
             if temp < nums[mid] {
+                // 防止出现 right = 0 - 1
+                if 0 == mid { break; }
                 right = mid - 1;
             } else {
                 left = mid + 1;
